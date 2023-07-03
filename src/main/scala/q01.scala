b27c77bb383db578b09e826c1d899ee10e73b027
chathura226 <chathuralakshan226@gmail.com>
2023-07-03 11:55:56 +0530

    q01

---
+++ src/main/scala/q01.scala
@@ -0,0 +1,14 @@
+object q01 extends App{
+  def interest(deposit:Double):Double={
+    deposit match{
+      case x if x<20000 => x*0.02
+      case x if x<200000 => x*0.04
+      case x if x<2000000 => x*0.035
+      case _ => deposit*0.065
+    }
+  }
+
+  print("Input deposit amount: ")
+  var deposit=scala.io.StdIn.readDouble()
+  println("Interest : "+interest(deposit))
+}
